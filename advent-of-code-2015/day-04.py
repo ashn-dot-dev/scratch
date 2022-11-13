49884d9db6fb1e7ae2020a7c38a7b737ad505c06
ashn <me@ashn.dev>
2022-11-12 21:33:49 -0500

    Add Advent of Code 2015 day 04

---
+++ advent-of-code-2015/day-04.py
@@ -0,0 +1,13 @@
+import hashlib
+import itertools
+
+INPUT="bgvyzdsv"
+
+def md5_that_starts_with_zeros(zeros):
+    for n in itertools.count(start=0):
+        hash = hashlib.md5(INPUT.encode() + str(n).encode()).hexdigest()
+        if hash.startswith("0" * zeros):
+            return n
+
+print(f"PART 1: {md5_that_starts_with_zeros(5)}")
+print(f"PART 2: {md5_that_starts_with_zeros(6)}")
