cc64977ae93451892649f666429d13df9a3454a8
ashn <me@ashn.dev>
2023-05-03 20:27:30 -0400

    Use an actual parser instead of eval for Advent of Code 2022 Day 13

--- advent-of-code-2022/day-13.py
+++ advent-of-code-2022/day-13.py
@@ -2,7 +2,121 @@ from argparse import ArgumentParser
 from itertools import count
 from enum import Enum
 
+
+class TokenKind(Enum):
+    EOI = "end-of-input"
+    COMMA = ","
+    INTEGER = "integer"
+    LBRACKET = "["
+    RBRACKET = "]"
+
+
+class Token:
+    def __init__(self, kind, text):
+        self.kind = kind
+        self.text = text
+
+    def __str__(self):
+        if self.kind == TokenKind.INTEGER:
+            return f"{self.kind.value}({self.text})"
+        return f"{self.kind.value}"
+
+
+class Lexer:
+    EOI = ""
+
+    def __init__(self, source):
+        self.source = source
+        self.position = 0
+
+    def is_eoi(self):
+        return self.position >= len(self.source)
+
+    def current_character(self):
+        if self.is_eoi():
+            return Lexer.EOI
+        return self.source[self.position]
+
+    def next_token(self):
+        start = self.position
+
+        if self.is_eoi():
+            return Token(TokenKind.EOI, Lexer.EOI)
+
+        if self.current_character() == TokenKind.COMMA.value:
+            self.position += len(TokenKind.COMMA.value)
+            return Token(TokenKind.COMMA, self.source[start : self.position])
+
+        if self.current_character() == TokenKind.LBRACKET.value:
+            self.position += len(TokenKind.LBRACKET.value)
+            return Token(TokenKind.LBRACKET, self.source[start : self.position])
+
+        if self.current_character() == TokenKind.RBRACKET.value:
+            self.position += len(TokenKind.RBRACKET.value)
+            return Token(TokenKind.RBRACKET, self.source[start : self.position])
+
+        if self.current_character().isdigit():
+            while self.current_character().isdigit():
+                self.position += 1
+            return Token(TokenKind.INTEGER, self.source[start : self.position])
+
+        raise Exception(f"invalid character `{self.current_character()}`")
+
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current = lexer.next_token()
+
+    def parse_packet(self):
+        value = self.parse_list()
+        if self.current.kind != TokenKind.EOI:
+            raise Exception(f"expected {TokenKind.EOI.value} (found `{self.current}`)")
+        return value
+
+    def parse_value(self):
+        if self.current.kind == TokenKind.LBRACKET:
+            return self.parse_list()
+
+        if self.current.kind == TokenKind.INTEGER:
+            return self.parse_integer()
+
+        raise Exception(f"expected value (found `{self.current}`)")
+
+    def parse_list(self):
+        if self.current.kind != TokenKind.LBRACKET:
+            raise Exception(f"expected `{TokenKind.LBRACKET.value}` (found `{self.current}`)")
+        self.current = self.lexer.next_token()
+
+        result = []
+        while self.current.kind != TokenKind.RBRACKET:
+            if len(result) != 0:
+                if self.current.kind != TokenKind.COMMA:
+                    raise Exception(f"expected `{TokenKind.COMMA.value}` (found `{self.current}`)")
+                self.current = self.lexer.next_token()
+            result.append(self.parse_value())
+
+        if self.current.kind != TokenKind.RBRACKET:
+            raise Exception(f"expected `{TokenKind.RBRACKET.value}` (found `{self.current}`)")
+        self.current = self.lexer.next_token()
+        return result
+
+    def parse_integer(self):
+        if self.current.kind != TokenKind.INTEGER:
+            raise Exception(f"expected {TokenKind.INTEGER.value} (found `{self.current}`)")
+        result = int(self.current.text)
+        self.current = self.lexer.next_token()
+        return result
+
+
+def parse_packet(line):
+    lexer = Lexer(line)
+    parser = Parser(lexer)
+    return parser.parse_packet()
+
+
 Order = Enum("Order", ["NEITHER", "INORDER", "UNORDER"])
+
 
 def compare(lhs, rhs, debug=False) -> Order:
     if debug:
@@ -53,7 +167,7 @@ def compare(lhs, rhs, debug=False) -> Order:
 
 def part1(input, debug=False):
     pairs = [tuple(x.split("\n")) for x in input.split("\n\n")]
-    pairs = [(eval(p[0]), eval(p[1])) for p in pairs]
+    pairs = [(parse_packet(p[0]), parse_packet(p[1])) for p in pairs]
 
     sum = 0
     for i in range(len(pairs)):
@@ -65,8 +179,9 @@ def part1(input, debug=False):
 
     print(f"PART 1: {sum}")
 
+
 def part2(input, debug=False):
-    packets = [eval(p) for p in input.split("\n") if len(p) != 0]
+    packets = [parse_packet(p) for p in input.split("\n") if len(p) != 0]
     packets.append([[2]])
     packets.append([[6]])
 
@@ -104,12 +219,13 @@ def part2(input, debug=False):
 
 def main(args):
     with open("day-13.input") as f:
-        input = f.read();
+        input = f.read()
 
     part1(input, args.debug)
     part2(input, args.debug)
 
+
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("-d", "--debug", action="store_true")
-    main(parser.parse_args())
+    ap = ArgumentParser()
+    ap.add_argument("-d", "--debug", action="store_true")
+    main(ap.parse_args())
